e9bd644292552160f5367e596d782d41bc1111e2
Shaquille Sookanlall <ST10140587@vcconnect.edu.za>
2024-05-29 09:37:01 +0200

    Created Recipe Steps

---
+++ Recipe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10140587_Prog6211_Part2
+{
+    internal class Recipe
+    {
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set;}
+        public List<String> Method { get; set;}
+        private List<String> OriginalIngredients { get; set;}
+        public double CalorieCount { get; set; }
+
+        public Recipe(string name)
+        {
+            Name = name;
+            Ingredients = new List<Ingredient>();
+            Method = new List<String>();
+            OriginalIngredients = new List<String>();
+        }
+    }
+}
