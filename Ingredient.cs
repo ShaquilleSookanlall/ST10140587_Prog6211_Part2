9a470e35cfffb06e941291b8ad8efb2358d23174
Shaquille Sookanlall <ST10140587@vcconnect.edu.za>
2024-05-29 09:32:46 +0200

    Created properties for class Ingredient

--- Ingredient.cs
+++ Ingredient.cs
@@ -8,5 +8,19 @@ namespace ST10140587_Prog6211_Part2
 {
     internal class Ingredient
     {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string UnitOfMeasurement {  get; set; }
+        public double Calories { get; set; }
+        public string FoodGroup { get; set; }
+
+        public Ingredient(string name, double quantity, string unitOfMeasurement, double calories, string foodGroup)
+        {
+            Name = name;
+            Quantity = quantity;
+            UnitOfMeasurement = unitOfMeasurement;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
     }
 }
