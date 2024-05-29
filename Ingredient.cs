f9bcfaafc324242b50003d42c0600aba98eb7f54
Shaquille Sookanlall <ST10140587@vcconnect.edu.za>
2024-05-29 09:30:31 +0200

    Created Ingredient,cs file

---
+++ Ingredient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10140587_Prog6211_Part2
+{
+    internal class Ingredient
+    {
+    }
+}
