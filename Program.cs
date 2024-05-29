e69f8b5865cc4b8aeb32c8d1be9384db7b3d2af4
Shaquille Sookanlall <ST10140587@vcconnect.edu.za>
2024-05-29 09:29:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10140587_Prog6211_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
